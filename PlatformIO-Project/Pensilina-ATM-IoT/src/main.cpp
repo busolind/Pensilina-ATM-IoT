2c300c581def2fcaf4bfca70faf3a734404179fd
Davide Busolin <davide.busolin@studenti.unimi.it>
2021-03-22 00:49:16 +0100

    Edited parse function and found a problem

--- PlatformIO-Project/Pensilina-ATM-IoT/src/main.cpp
+++ PlatformIO-Project/Pensilina-ATM-IoT/src/main.cpp
@@ -4,20 +4,28 @@
 #include <ESP8266WiFi.h>
 #include <TaskScheduler.h>
 #include <WiFiClientSecureBearSSL.h>
+#include <list>
 
 const char *ssid = "IOT_TEST";
 const char *password = "IOT_TEST";
 
 String apiUrl = "https://giromilano.atm.it/proxy.ashx";
-String stopCode = "12493";
+String stopCode = "14033";
+
+#define LINES_SIZE 10
 
 // Expires On	Thursday, 28 July 2022 at 14:00:00
 // const char fingerprint[] PROGMEM = "72 84 14 05 5A 4B 27 DD 07 44 FC 00 96 4E 9B 06 42 0B 9C 7F";
 // pare funzionare anche senza fingerprint
 
 String stopJSON = "";
-String lineId = "";
-String waitMessage = "";
+String *lineIds;
+String *waitMessages;
+
+unsigned long last_action = 0;
+
+// ToDo:
+//std::list<String> bulletins;
 
 void setup_wifi() {
   delay(10);
@@ -84,15 +92,31 @@ void https_request() {
 
 void parse_stopJSON() {
   if (stopJSON != "") {
-    DynamicJsonDocument doc(3000);
+    DynamicJsonDocument doc(10000);
     DeserializationError error = deserializeJson(doc, stopJSON);
 
     if (error) {
       Serial.print(F("deserializeJson() failed: "));
       Serial.println(error.f_str());
     } else {
-      lineId = doc["Lines"][0]["Line"]["LineId"].as<String>();
-      waitMessage = doc["Lines"][0]["WaitMessage"].as<String>();
+      JsonArray linesArray = doc["Lines"].as<JsonArray>();
+
+      lineIds = new String[LINES_SIZE];
+      waitMessages = new String[LINES_SIZE];
+
+      int i = 0;
+      for (JsonVariant line : linesArray) {
+        Serial.print(line["Line"]["LineId"].as<String>() + "\t");
+        Serial.println(line["WaitMessage"].as<String>());
+
+        lineIds[i] = line["Line"]["LineId"].as<String>();
+        waitMessages[i] = line["WaitMessage"].as<String>();
+
+        i++;
+      }
+
+      //lineId = doc["Lines"][0]["Line"]["LineId"].as<String>();
+      //waitMessage = doc["Lines"][0]["WaitMessage"].as<String>();
     }
   }
 }
@@ -103,10 +127,17 @@ void setup() {
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    https_request();
+  unsigned long now = millis();
+  if (now - last_action > 15000) {
+    last_action = now;
+    if (WiFi.status() == WL_CONNECTED) {
+      https_request();
+    }
+    parse_stopJSON();
+    //Serial.println(lineId + "\t" + waitMessage);
+    Serial.print("Free heap: ");
+    Serial.println(ESP.getFreeHeap());
   }
-  parse_stopJSON();
-  Serial.println(lineId + "\t" + waitMessage);
-  delay(15000);
 }
+
+//PROBLEM: this stops working after a while, the response stops being printed and the JSON stops being parsed
