9437c33313ae6191ce3361b3e3b34803582e182e
Davide Busolin <davide.busolin@studenti.unimi.it>
2021-03-23 18:20:04 +0100

    Finally got streams (kind of) working

--- PlatformIO-Project/Pensilina-ATM-IoT/src/main.cpp
+++ PlatformIO-Project/Pensilina-ATM-IoT/src/main.cpp
@@ -3,6 +3,7 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <SSD1306Wire.h> //https://github.com/ThingPulse/esp8266-oled-ssd1306
+#include <StreamUtils.h>
 #include <TaskScheduler.h>
 #include <WiFiClientSecureBearSSL.h>
 #include <list>
@@ -13,8 +14,8 @@ const char *password = "IOT_TEST";
 String apiUrl = "https://giromilano.atm.it/proxy.ashx";
 //String stopCode = "12493"; //Via Celoria (Istituto Besta)
 //String stopCode = "14033"; //Via Bellini V.le Casiraghi (Sesto S.G.)
-//String stopCode = "11492"; //VERY LONG response, tram
-String stopCode = "12587"; //Sesto Marelli M1 dir Bicocca
+String stopCode = "11492"; //VERY LONG response, tram
+//String stopCode = "12587"; //Sesto Marelli M1 dir Bicocca
 
 // Expires On	Thursday, 28 July 2022 at 14:00:00
 // const char fingerprint[] PROGMEM = "72 84 14 05 5A 4B 27 DD 07 44 FC 00 96 4E 9B 06 42 0B 9C 7F";
@@ -35,6 +36,8 @@ unsigned long last_action = 0;
 //std::list<String> bulletins;
 
 //HTTPS STREAM??
+
+BearSSL::WiFiClientSecure client;
 
 void setup_wifi() {
   delay(10);
@@ -94,18 +97,62 @@ void parse_stopJSON(String stopJSON) {
   }
 }
 
+void parse_stopJSON_stream(BearSSL::WiFiClientSecure &str_client) {
+
+  StaticJsonDocument<200> filter;
+  filter["Lines"][0]["Line"]["LineId"] = true;
+  filter["Lines"][0]["WaitMessage"] = true;
+
+  //ReadLoggingStream loggingStream(str_client, Serial);
+
+  ReadBufferingStream bufferingStream(str_client, 64);
+
+  //Skip to start of JSON (necessary because of some numeric value on first line of the stream. My use of the stream is not ideal)
+  while (char(bufferingStream.peek()) != '{') {
+    bufferingStream.read();
+  }
+
+  /*
+  while (bufferingStream.available()) {
+    Serial.print(char(bufferingStream.read()));
+  }
+*/
+
+  StaticJsonDocument<1000> doc;
+  DeserializationError error = deserializeJson(doc, bufferingStream, DeserializationOption::Filter(filter));
+
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+  } else {
+    JsonArray linesArray = doc["Lines"].as<JsonArray>();
+
+    lineIds.clear();
+    waitMessages.clear();
+
+    for (JsonVariant line : linesArray) {
+      Serial.print(line["Line"]["LineId"].as<String>() + "\t");
+      Serial.println(line["WaitMessage"].as<String>());
+
+      String lineId = line["Line"]["LineId"].as<String>();
+      String waitMessage = line["WaitMessage"].as<String>();
+
+      lineIds.push_back(lineId);
+      waitMessages.push_back(waitMessage);
+    }
+  }
+}
+
 void https_request() {
   String postPayload = "url=tpPortal/geodata/pois/stops/" + stopCode;
 
-  std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
-
   //client->setFingerprint(fingerprint);
-  client->setInsecure();
+  client.setInsecure();
 
   HTTPClient https;
 
   Serial.print("[HTTPS] begin...\n");
-  if (https.begin(*client, apiUrl)) { // HTTPS
+  if (https.begin(client, apiUrl)) { // HTTPS
 
     https.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
@@ -120,11 +167,10 @@ void https_request() {
 
       // file found at server
       if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY || httpCode == 400) {
-        //String payload = https.getString();
-        //https.ge
-        String stopJSON = https.getString();
-        Serial.println(stopJSON);
-        parse_stopJSON(stopJSON);
+        //String stopJSON = https.getString();
+        //Serial.println(stopJSON);
+        //parse_stopJSON(stopJSON);
+        parse_stopJSON_stream(client);
       }
     } else {
       Serial.printf("failed, error: %s\n", https.errorToString(httpCode).c_str());
